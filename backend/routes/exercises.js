29e06a356e1b3ccb2d9b0a07363abad6f8565ad2
bennyekop <bennyekop4@gmail.com>
2019-08-03 12:25:18 +0700

    start init frontend Router

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,32 @@ router.route('/add').post((req, res) => {
   .catch((err) => req.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch((err) => req.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise telah dihapus..!'))
+    .catch((err) => req.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise telah diupdate'))
+        .catch((err) => req.status(400).json('Error: ' + err));
+    })
+    .catch((err) => req.status(400).json('Error: ' + err));
+});
+
+
 module.exports = router;
